6f24cd84b84ceef720161d9c34d9664933093fe0
Bulat <bulat.ahmetzyanoff@yandex.ru>
2018-10-18 22:10:06 +0300

    miscalculates function summ_tugrigs

--- Computer line/main.py
+++ Computer line/main.py
@@ -45,9 +45,28 @@ def coordinates_isvalid(coordinates):
     return False
 
 
-def summ_tugrigs(coordinates):
-    pass
+def summ_tugrigs(provider_coordinate, pig_coordinates, tugtic):
 
+    result = 0
+
+    provide_x = provider_coordinate[0]
+    provide_y = provider_coordinate[1]
+
+    for pig in pig_coordinates:
+
+        pig_x = pig[0]
+        pig_y = pig[1]
+
+        result += ( fabs(int(provide_x)) - fabs(int(pig_x)) ) + ( fabs(int(provide_y)) - fabs(int(pig_y)) )
+
+        print(pig_x, pig_y, result)
+
+    return result
+
+def do_exit(string):
+    if str(string).lower == 'exit':
+        return True
+    return False
 
 if __name__ == '__main__':
 
@@ -64,18 +83,21 @@ if __name__ == '__main__':
     while 1:
 
         count_pig = input('\tCount pig: ')
+        if (do_exit(count_pig)): break
         if ( N_isvalid(count_pig) ):
             break
 
     while 1:
 
         count_rouble_need = input('\tCount rouble for one LAN: ')
+        if (do_exit(count_rouble_need)): break
         if ( rouble_need(count_rouble_need) ):
             break
 
     while 1:
 
         count_rouble_have = input('\tCount rouble they have: ')
+        if (do_exit(count_rouble_have) ): break
         if ( rouble_have(count_rouble_have) ):
             break
 
@@ -89,12 +111,15 @@ if __name__ == '__main__':
         try:
 
             x = input('\tEnter "x" coordinate: ')
+            if (do_exit(x)): break
             if coordinates_isvalid(x):
 
                 y = input('\tEnter "y" coordinate: ')
+                if (do_exit(y)): break
                 if coordinates_isvalid(y):
 
-                    provider_coordinate.append([x, y])
+                    provider_coordinate.append(x)
+                    provider_coordinate.append(y)
                     break
 
         except:
@@ -117,14 +142,19 @@ if __name__ == '__main__':
             try:
 
                 x = input('\tEnter "x" coordinate: ')
+                if (do_exit(x)): break
                 if coordinates_isvalid(x):
 
                     y = input('\tEnter "y" coordinate: ')
                     if coordinates_isvalid(y):
-
+                        if (do_exit(y)): break
                         pig_coordinates.append([x, y])
                         break
 
             except:
                 error = True
                 continue
+
+
+    tugrigs = summ_tugrigs(provider_coordinate, pig_coordinates, count_rouble_need)
+    print(tugrigs )
