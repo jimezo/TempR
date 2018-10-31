1fbe31e62fa4a5a192ca8deb02914045e9d66b53
Bulat <bulat.ahmetzyanoff@yandex.ru>
2018-11-01 01:19:02 +0300

    Greedy algorithm V0.1.1 On this version has been added function 'start_algorithm' in order to run script Function requires improvements

--- Greedy algorith/main.py
+++ Greedy algorith/main.py
@@ -1,13 +1,15 @@
-
 class GreedyAlgorith:
 
+    # Function contractor
     def __init__(self, size = 0):
 
-        self.size = size
+        self.size = size # Variable there save matrix size
 
-        self.matrix = []
+        self.matrix = [] # List there save matrix
 
-        self.min_values = []
+        self.min_values = {}
+
+        self.optimal_way = {}
 
 
     def set_matrix(self):
@@ -18,6 +20,7 @@ class GreedyAlgorith:
 
             for rows in range( self.size ):
 
+                # Cols and rows not can cross yorself, so, add 0 to matrix and pass next value
                 if cols == rows:
 
                     row.append(0)
@@ -31,10 +34,12 @@ class GreedyAlgorith:
             self.matrix.append( row )
 
 
+    # This function find minimal value from matrix and return dict with minimal value
     def get_min(self):
 
         for cols in range( self.size ):
 
+            # Assign only one rows from matrix to variable min
             min = max( self.matrix[cols] )
 
             for rows in range( self.size ):
@@ -47,11 +52,36 @@ class GreedyAlgorith:
 
                     min = self.matrix[cols][rows]
 
-            self.min_values.append( min )
+                if rows + 1 == size:
 
+                    # Save minimal value and him coordinates(cols, rows) in dict
+                    self.min_values[min] = [cols, rows]
+
+        # Return dict with minimal values
         return self.min_values
 
 
+    # Function for run algorithm
+    def start_algorithm(self, depth = 0):
+
+        # Exit from function if dict with minimal values is empty
+        if self.min_values == None:
+
+            return
+
+        # Assign minimal key from dict
+        min_key = min(self.min_values.keys())
+
+        key, value = self.min_values[min_key]
+
+        # Delete element from dict
+        del self.min_values[min_key]
+
+        summ = sum( self.matrix[ depth ] ) - self.matrix[ value[0], value[1] ]
+
+
+
+    # This function show matrix on display
     def show_matrix(self):
 
         print ( self.matrix )
