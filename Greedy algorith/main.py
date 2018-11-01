aa0191bc4add420d26e1eae88f4d1ccbcb4713ba
Bulat <bulat.ahmetzyanoff@yandex.ru>
2018-11-01 22:05:50 +0300

    Greedy algorithm V0.1.4 Has been added one function: 'find_evaluation' Has been changed two function: 'get_min' and 'start_algorithm'

--- Greedy algorith/main.py
+++ Greedy algorith/main.py
@@ -3,22 +3,13 @@ import math # math.inf
 class GreedyAlgorith:
 
     # Function contractor
-    def __init__(self, size = 0):
+    def __init__(self, matrix):
 
-        self.size = size # Variable there save matrix size
+        self.size = 0 # Variable there save matrix size
 
-        self.matrix = [        # List there save matrix
+        self.matrix = matrix # List there save matrix
 
-                    [ math.inf, 5, 11, 9 ],
-                    [ 10, math.inf, 8, 7 ],
-                    [ 7, 14, math.inf, 8 ],
-                    [ 12, 6, 15, math.inf ],
-
-                ]
-
-        self.min_in_rows = []
-
-        self.min_in_cols = []
+        self.min_values = []
 
         self.optimal_way = {}
 
@@ -45,31 +36,33 @@ class GreedyAlgorith:
             self.matrix.append( row )
 
 
-    def reduction_rows(self, matrix):
+    def reduction_rows(self, matrix, min_in_rows):
 
         for rows in range( self.size ):
 
             for cols in range( self.size ):
 
-                matrix[rows][cols] -= self.min_in_rows[rows]
+                matrix[rows][cols] -= min_in_rows[rows]
 
         return matrix
 
 
-    def reduction_cols(self, matrix):
+    def reduction_cols(self, matrix, min_in_cols):
 
         for rows in range( self.size ):
 
             for cols in range( self.size ):
 
-                matrix[cols][rows] -= self.min_in_cols[rows]
+                matrix[cols][rows] -= min_in_cols[rows]
 
         return matrix
-        
+
 
     def find_null_values( self, matrix ):
 
         null_values = {}
+
+        count = 0
 
         for rows in range( self.size ):
 
@@ -79,26 +72,68 @@ class GreedyAlgorith:
 
                     val = matrix[rows][cols]
 
-                    null_values[ 'val_' + str(rows) ] = [ rows, cols ]
+                    null_values[ 'val_' + str(count) ] = [ rows, cols ]
+
+                    count += 1
 
         return null_values
+
+
+    # Function for calculate evaluation from rows and column where matrix element == 0
+    def find_evaluation(self, matrix, null_values):
+
+        min_in_rows = []
+
+        min_in_cols = []
+
+        evaluations = {}
+
+        count = 0
+
+        for val in null_values.values():
+
+            min_in_rows.clear()
+
+            min_in_cols.clear()
+
+            for elem in range( self.size ):
+
+                if elem != val[1]:
+
+                    min_in_rows.append( matrix[ val[0] ][elem] )
+
+                if elem != val[0]:
+
+                    min_in_cols.append( matrix[ elem ][ val[1] ] )
+
+            min_val_col = min( min_in_cols )
+
+            min_val_row = min( min_in_rows )
+
+            eval = min_val_col + min_val_row
+
+            evaluations[ count ] = {'eval': eval, 'pos': [val]}
+
+            count += 1
+
+        return evaluations
 
 
     # This function find minimal value from matrix and return dict with minimal value
     def get_min(self):
 
-        if self.size == 0:
+        min_in_rows = []
 
-            self.size = len(self.matrix)
+        min_in_cols = []
 
         matrix = self.matrix.copy()
 
         # Find minimal values from rows
         for rows in range( self.size ):
 
-            self.min_in_rows.append( min( matrix[rows] ) )
+            min_in_rows.append( min( matrix[rows] ) )
 
-        matrix = self.reduction_rows( matrix )
+        matrix = self.reduction_rows( matrix, min_in_rows)
 
         for rows in range( self.size ):
 
@@ -108,24 +143,34 @@ class GreedyAlgorith:
 
                 col.append( matrix[cols][rows] )
 
-            self.min_in_cols.append( min(col) )
+            min_in_cols.append( min(col) )
 
-        matrix = self.reduction_cols( matrix )
+        matrix = self.reduction_cols( matrix, min_in_cols)
 
-        null_values = self.find_null_values( matrix )
+        self.min_values.append( min_in_rows )
+        self.min_values.append( min_in_cols )
 
-        print("Changed matrix: ", matrix)
-        print ("Min in rows: ", self.min_in_rows)
-        print ("Min in cols: ", self.min_in_cols)
-        print("Null values: ", null_values )
+        return matrix
 
 
     # Function for run algorithm
     def start_algorithm(self):
 
-        matrix.show_matrix()
+        self.size = len( self.matrix )
 
-        matrix.get_min()
+        self.show_matrix()
+
+        matrix = self.get_min()
+
+        null_values = self.find_null_values( matrix )
+
+        evaluation = self.find_evaluation(matrix, null_values)
+
+        print("Changed matrix: ", matrix)
+        print ("Min in rows: ", self.min_values[0])
+        print ("Min in cols: ", self.min_values[1])
+        print("Null values: ", null_values )
+        print("Evaluatons: ", evaluation)
 
 
     # This function show matrix on display
@@ -136,6 +181,15 @@ class GreedyAlgorith:
 
 if __name__ == "__main__":
 
-    matrix = GreedyAlgorith()
+    M = [
+
+        [ math.inf, 5, 11, 9 ],
+        [ 10, math.inf, 8, 7 ],
+        [ 7, 14, math.inf, 8 ],
+        [ 12, 6, 15, math.inf ],
+
+    ]
+
+    matrix = GreedyAlgorith(M)
 
     matrix.start_algorithm()
